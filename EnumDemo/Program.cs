37a8677b8836d68d912f4dc69659ad55e03acc72
Vincent Kaveh <vincent.kaveh@iths.se>
2023-09-11 10:29:02 +0200

    Added two more write lines again.

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -7,6 +7,9 @@
             Console.WriteLine("Hello, Vincent!");
             Console.WriteLine("Cloud23 baby");
             Console.WriteLine("Version 2 is now live");
+            
+            Console.WriteLine("Rad 3");
+            Console.WriteLine("Rad 4");
         }
     }
 }
